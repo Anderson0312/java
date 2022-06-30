38f8cab8f2cdc91bd0ba4947412eba5466c5dbb4
Anderson0312 <andersonmoura812@gmail.com>
2022-06-29 21:32:20 -0300

    att

---
+++ test/src/listex4/ex1/Interface.java
@@ -0,0 +1,8 @@
+package listex4.ex1;
+
+public interface Interface {
+    public abstract void valorLocacao();
+    public abstract void valorImposto();
+    public abstract void valorPagar();
+    
+}
---
+++ test/src/listex4/ex1/Locadora.java
@@ -0,0 +1,39 @@
+package listex4.ex1;
+
+public class Locadora implements Interface{
+    public String Mcarro;
+    public String Inicio;
+    public String Final;
+
+    public void valorLocacao() {
+
+    }
+
+    public void valorImposto() {
+
+    }
+
+    public void valorPagar() {
+
+    }
+
+    public String getMcarro() {
+        return Mcarro;
+    }
+    public void setMcarro(String mcarro) {
+        Mcarro = mcarro;
+    }
+    public String getInicio() {
+        return Inicio;
+    }
+    public void setInicio(String inicio) {
+        Inicio = inicio;
+    }
+    public String getFinal() {
+        return Final;
+    }
+    public void setFinal(String final1) {
+        Final = final1;
+    }
+    
+}
